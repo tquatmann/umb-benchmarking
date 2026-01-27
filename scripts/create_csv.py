b6ebf323e9f8984b40a818f54e7650331dd4690b
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2026-01-27 23:19:25 +0100

    Up

--- scripts/create_csv.py
+++ scripts/create_csv.py
@@ -1,17 +1,55 @@
 import os,sys
+from collections import OrderedDict
 
 IMPORT_TIME = "import_time"
 EXPORT_TIME = "export_time"
 EXPORT_SIZE = "export_size"
-WALL_TIME = "wall_time"
+FULL_TIME = "full_time"
 
-def save_csv(csv, path : str):
-    with open(path, 'w') as csv_file:
+def save_csv(csv, name : str):
+    with open(name + ".csv", 'w') as csv_file:
         for line in csv:
             line_strs = [str(x) for x in line]
             csv_file.write("\t".join(line_strs) + "\n")
-    print("Saved results invocations to '{}'.".format(path))
+    print("Saved results invocations to '{}'.".format(name + ".csv"))
 
+def avg_csv(csv, name : str):
+    a = [csv[0]]  # header
+    for row in csv[1:]:
+        new_row = [row[0]]  # first column is the model name
+        for entry in row[1:]:
+            if entry == "N/A":
+                new_row.append("")
+            else:
+                values = [float(x) for x in entry if x != "N/A"]
+                if len(values) == 0:
+                    new_row.append("")
+                else:
+                    avg_value = sum(values) / len(values)
+                    new_row.append(avg_value)
+        a.append(new_row)
+    save_csv(a, name + "_avg")
+    return a
+
+def quantile_csv(csv, name : str, min_value : float = 0.1):
+    columns = [[]] * len(csv[0][1:])
+    for row in csv[1:]:
+        for i, entry in enumerate(row[1:]):
+            if entry == "":
+                continue
+            columns[i] = columns[i] + [entry]
+    for i in range(len(columns)):
+        columns[i].sort()
+    output = [["n"] + csv[0][1:]]
+    for n in range(len(csv) - 1):
+        line = [n + 1]
+        for m in range(len(columns)):
+            if n >= len(columns[m]):
+                line.append("")
+            else:
+                line.append(columns[m][n])
+        output.append(line)
+    save_csv(output, name + "_quantile")
 
 def parse_float(text : str, before : str, after : str):
     start = text.find(before)
@@ -27,23 +65,19 @@ def parse_float(text : str, before : str, after : str):
         return value
     except:
         return None
+def parse_float_or_zero(text : str, before : str, after : str):
+    value = parse_float(text, before, after)
+    if value is None:
+        return 0.0
+    return value
 
 def parse_walltime(log : str):
     return parse_float(log, "Wallclock time: ", " seconds")
 
 def parse_prism_log(log : str, what : str):
-    if "Return code: None (timeout)" in log:
-        return "TO"
-    memouts = ["java.lang.OutOfMemoryError", "IllegalArgumentException: Illegal Capacity: -1"]
-    if any(memout in log for memout in memouts):
-        return "MO"
-    if "Error: Currently, the sparse engine cannot handle models with more than 2147483647 states," in log:
-        return "ERR"
-    err_pos = log.find("File does not exist.")
-    if err_pos > 0:
-        other_pos = log.find("rew:\tFile does not exist.")
-        if other_pos == -1 or other_pos > err_pos:
-            raise AssertionError("File does not exist." not in log, "Log indicates missing file: {}".format(log))
+    if what == FULL_TIME:
+        if "Time for model checking" in log:
+            return parse_walltime(log)
     if what == IMPORT_TIME:
         return parse_float(log, "Time for model construction: ", "seconds.")
     elif what == EXPORT_TIME:
@@ -54,40 +88,32 @@ def parse_prism_log(log : str, what : str):
         while "Size of output file is " in sublog:
             size += parse_float(sublog, "Size of output file is ", " bytes")
             sublog = sublog[sublog.find("Size of output file is ") + 1:]
-        return size
-    assert False, "Log parsing for {} in  prism not successful: {}".format(what, log)
+        return size if size > 0 else None
 
 def parse_modest_log(log : str, what : str):
-    if "Return code: None (timeout)" in log:
-        return "TO"
-    memouts = ["error: Out of memory."]
-    if any(memout in log for memout in memouts):
-        return "MO"
-    if "oscillators" in log and "Return code: -9" in log:
-        return "ERR" # special case for known issue
-    if "oscillators" in log and "Return code: -6" in log:
-        return "ERR" # special case for known issue
-    if "kanban" in log and "Return code: -6" in log:
-        return "ERR" # special case for known issue
-    if "fms" in log and "Return code: -6" in log:
-        return "ERR" # special case for known issue
-    if "pacman.100" in log and "Return code: -9" in log:
-        return "MO" # special case for known mem out
-    if "Complex initial states specifications are not yet supported." in log:
-        return "ERR"
-    assert "File does not exist." not in log, "Log indicates missing file: {}".format(log)
-    if what == IMPORT_TIME:
-        assert "+ State space exploration" in log, "Unexpected modest log format: {}".format(log)
-        sublog = log[log.find("+ State space exploration"):]
+    if what == FULL_TIME:
+        pos1 = log.find("+ Property")
+        if pos1 != -1 and log.find("Time", pos1) != -1:
+            return parse_walltime(log)
+    elif what == IMPORT_TIME:
+        start_pos = log.find("+ State space")
+        if start_pos == -1:
+            return None
+        sublog = log[start_pos:]
         sublog = sublog[:sublog.find("\n\n")]
-        importtime = parse_float(sublog, "Time (exploration): ", " s")
-        importtime += parse_float(sublog, "Time (merging): ", " s")
+        assert "Time" in sublog, "Unexpected modest log format: {}".format(log)
+        importtime = parse_float_or_zero(sublog, "Time (exploration): ", " s")
+        importtime += parse_float_or_zero(sublog, "Time (merging): ", " s")
+        importtime += parse_float_or_zero(sublog, "Time (decompress): ", " s")
+        importtime += parse_float_or_zero(sublog, "Time (validate): ", " s")
+        importtime += parse_float_or_zero(sublog, "Time (load): ", " s")
         return importtime
     elif what == EXPORT_TIME:
         start_pos = log.find("+ UMB export")
         if start_pos == -1:
             start_pos = log.find("+ Export to ")
-        assert start_pos != -1, "Unexpected modest log format: {}".format(log)
+        if start_pos == -1:
+            return None
         sublog = log[start_pos:]
         exporttime = parse_float(sublog, "Time: ", " s")
         return exporttime
@@ -97,27 +123,22 @@ def parse_modest_log(log : str, what : str):
         while "Size of output file is " in sublog:
             size += parse_float(sublog, "Size of output file is ", " bytes")
             sublog = sublog[sublog.find("Size of output file is ") + 1:]
-        return size
-    assert False, "Log parsing for modest not successful: {}".format(log)
+        return size if size > 0 else None
 
 
 def parse_storm_log(log : str, what : str):
-    if "Return code: None (timeout)" in log:
-        return "TO"
-    memouts = ["Maximum memory exceeded.", "The message of this exception is: std::bad_alloc"]
-    if any(memout in log for memout in memouts):
-        return "MO"
-    if "Return code: -9" in log and ("bluetooth.1" in log or "rabin.10" in log or "pnueli-zuck.10" in log):
-        return "MO"
-    assert "File does not exist." not in log, "Log indicates missing file: {}".format(log)
-    jani_parsing = parse_float(log, "Time for model input parsing: ", "s.\n")
+    jani_parsing = parse_float_or_zero(log, "Time for model input parsing: ", "s.\n")
     construction = parse_float(log, "Time for model construction: ", "s.\n")
-    preprocessing = parse_float(log, "Time for model preprocessing: ", "s.\n")
+    preprocessing = parse_float_or_zero(log, "Time for model preprocessing: ", "s.\n")
     export = parse_float(log, "Time for model export: ", "s.\n")
-    if what == IMPORT_TIME:
+    if what == FULL_TIME:
+        if "Time for model checking" in log:
+            return parse_walltime(log)
+    elif what == IMPORT_TIME:
         if construction is None:
             return None
-        return construction + (preprocessing if preprocessing is not None else 0.0) + (jani_parsing if jani_parsing is not None else 0.0)
+        else:
+            return jani_parsing + construction + preprocessing
     elif what == EXPORT_TIME:
         return export
     elif what == EXPORT_SIZE:
@@ -126,7 +147,7 @@ def parse_storm_log(log : str, what : str):
         while "Size of output file is " in sublog:
             size += parse_float(sublog, "Size of output file is ", " bytes")
             sublog = sublog[sublog.find("Size of output file is ") + 1:]
-        return size
+        return size if size > 0 else None
 
 def create_csv(log_dir : str, what : str, not_available_str : str = "N/A"):
     row_headers = []
@@ -143,7 +164,7 @@ def create_csv(log_dir : str, what : str, not_available_str : str = "N/A"):
         src_format = parts[1]
         task = parts[2]
         cfg = parts[3]
-        if what in [IMPORT_TIME, WALL_TIME] and task.startswith("to-"):
+        if what in [IMPORT_TIME, FULL_TIME] and task.startswith("to-"):
             continue
         if what in [EXPORT_TIME, EXPORT_SIZE] and not task.startswith("to-"):
             continue
@@ -162,9 +183,7 @@ def create_csv(log_dir : str, what : str, not_available_str : str = "N/A"):
             contents[row][column].append("")
         with open(os.path.join(log_dir, logfile), "r") as f:
             log_content = f.read()
-        if what == WALL_TIME:
-            value = parse_walltime(log_content)
-        elif tool == "storm":
+        if tool == "storm":
             value = parse_storm_log(log_content, what)
         elif tool == "prism":
             value = parse_prism_log(log_content, what)
@@ -196,16 +215,23 @@ if __name__ == "__main__":
         sys.exit(1)
     log_dir = sys.argv[1]
 
+    # raw data
     import_csv = create_csv(log_dir, IMPORT_TIME)
-    save_csv(import_csv, "import_times.csv")
-    import_csv = create_csv(log_dir, WALL_TIME)
-    save_csv(import_csv, "wall_times.csv")
+    save_csv(import_csv, IMPORT_TIME)
+    imp_t = avg_csv(import_csv, IMPORT_TIME)
+    full_csv = create_csv(log_dir, FULL_TIME)
+    save_csv(full_csv, FULL_TIME)
+    full_t = avg_csv(full_csv, FULL_TIME)
     export_csv = create_csv(log_dir, EXPORT_TIME)
-    save_csv(export_csv, "export_times.csv")
-    export_csv = create_csv(log_dir, EXPORT_SIZE)
-    save_csv(export_csv, "export_sizes.csv")
+    save_csv(export_csv, EXPORT_TIME)
+    exp_t = avg_csv(export_csv, EXPORT_TIME)
+    size_csv = create_csv(log_dir, EXPORT_SIZE)
+    save_csv(size_csv, EXPORT_SIZE)
+    size = avg_csv(size_csv, EXPORT_SIZE)
 
-
-
-
+    # quantile
+    quantile_csv(imp_t, IMPORT_TIME)
+    quantile_csv(full_t, FULL_TIME)
+    quantile_csv(exp_t, EXPORT_TIME)
+    quantile_csv(size, EXPORT_SIZE,1024)
 
